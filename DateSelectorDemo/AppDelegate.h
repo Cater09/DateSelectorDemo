76b335bc3868b5357041fa05c8d6297f5995a8d7
Ethan <Ethan@EthandeMac.local>
2017-08-22 03:33:32 +0800

    Initial Commit

---
+++ DateSelectorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DateSelectorDemo
+//
+//  Created by Ethan on 2017/8/22.
+//  Copyright © 2017年 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DateSelectorDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DateSelectorDemo
+//
+//  Created by Ethan on 2017/8/22.
+//  Copyright © 2017年 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
